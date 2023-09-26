ea67e84187e1b226a3791d4d61ab8dae0f7aeb84
ndkuma-01 <nikhildkumar7@gmail.com>
2023-09-26 12:35:29 -0500

    Added Concat

---
+++ Easy/Strings/Concat.java
@@ -0,0 +1,20 @@
+package Easy.Strings;
+
+public class Concat{
+  //READ ME
+  // 1. Create a method called concat that takes two strings and returns their concatenation
+  // 2. Then uncomment the code in the main branch and run it
+  // 3. Get all test cases to say true
+  // Note
+    // "a","b" -> "ab"
+  // Make sure the method is called concat
+
+  public static void main(String[] args) {
+  //   System.out.println(concat("a", "b").equals("ab"));
+  //   System.out.println(concat("1","2").equals("12"));
+  //   System.out.println(concat("12l2kn.,,", "2kms00ls...").equals("12l2kn.,,2kms00ls..."));
+  }
+
+  //  VVV Make your method below VVV
+
+}
