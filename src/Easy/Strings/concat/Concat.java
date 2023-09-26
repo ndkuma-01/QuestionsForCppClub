f3bbbee5fd7fa84c1e9dda5d89ce0cc2caa60d28
Kumar <nikhil.kumar018@topper.wku.edu>
2023-09-26 10:46:37 -0500

    Hello World

---
+++ src/Easy/Strings/concat/Concat.java
@@ -0,0 +1,21 @@
+package Easy.Strings.concat;
+
+public class Concat {
+    //  READ_ME
+    // 1. create a method to take two strings and return them concatenated:
+    // 2. Uncomment the code in the main method and run.
+    // 3. If all the test cases are true, then congrats!
+    // concat("a", "b") -> "ab"
+    // NOTES:
+    // 1. Make sure to name the method concat
+    public static void main(String[] args) {
+
+//        System.out.println("----TEST CASES---");
+//        System.out.println(concat("a", "b").equals("ab"));
+//        System.out.println(concat("1", "2").equals("12"));
+//        System.out.println(concat("5, 4 ,3 ,2 ,,,,", "3,2 1, ;;;").equals("5, 4 ,3 ,2 ,,,,3,2 1, ;;;"));
+
+    }
+
+
+}
